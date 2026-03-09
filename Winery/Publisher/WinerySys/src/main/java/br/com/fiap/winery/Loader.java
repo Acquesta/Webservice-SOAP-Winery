8c33cb48a542fc0e1adf66879cefe51b2633505e
Hellen Silva <hellen.aparecidams@outlook.com>
2026-03-09 19:53:48 -0300

    refactor: remove comentários dos arquivos principais

--- Winery/Publisher/WinerySys/src/main/java/br/com/fiap/winery/Loader.java
+++ Winery/Publisher/WinerySys/src/main/java/br/com/fiap/winery/Loader.java
@@ -4,15 +4,12 @@ import jakarta.xml.ws.Endpoint;
 
 public class Loader {
     public static void main(String[] args) {
-        // Instancia objeto WineStockServiceImplementation
         WineStockServiceImplementation wineStock = new WineStockServiceImplementation();
         
-        // Publica o serviço em http://localhost:8085/WineStockService
         Endpoint.publish("http://localhost:8085/WineStockService", wineStock);
         
         System.out.println("Serviço publicado!");
         
-        // Mantém o programa rodando
         System.out.println("Pressione Ctrl+C para parar o servidor...");
         try {
             Thread.sleep(Long.MAX_VALUE);
